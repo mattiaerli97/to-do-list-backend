9a23cb24bfb7a079b9b4a3d111e1d21e670b22fa
Mattia Erli <YYI4002@infocert.it>
2022-10-10 22:17:28 +0200

    heroku deploy

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import db from "./config.js";
 import taskRoutes from "./routes.js";
 import cors from "cors";
 
+const port = process.env.port || 8000;
 const app = express();
 
 /* try {
@@ -16,6 +17,6 @@ app.use(cors());
 app.use(express.json());
 app.use('/tasks', taskRoutes);
 
-app.listen(8080, () => console.log('Server running at port 8080'));
+app.listen(port, () => console.log('Server running at port ' + port));
 
 export default app;
