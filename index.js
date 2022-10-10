7397c1e8420d8339511c6f2ef984f1944c3deebb
Mattia Erli <YYI4002@infocert.it>
2022-10-10 18:04:16 +0200

    first commit

--- index.js
+++ index.js
@@ -17,3 +17,5 @@ app.use(express.json());
 app.use('/tasks', taskRoutes);
 
 app.listen(8080, () => console.log('Server running at port 8080'));
+
+export default app;
---
+++ test/test.js
@@ -0,0 +1,8 @@
+import request from "supertest";
+import app from "../index";
+
+describe("GET /", () => {
+    it("responds with Hello World!", (done) => {
+        request(app).get("/").expect("Hello World!", done);
+    })
+});
