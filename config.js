89dd7c93b458f53bb545465a3e5920a4952c6e32
Mattia Erli <YYI4002@infocert.it>
2022-10-11 15:59:29 +0200

    fix db host

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 import mysql from "mysql2";
 
 const db = mysql.createConnection({
-  host: "sql.freedb.tech",
-  user: "freedb_sql8525571",
-  password: "sVQDX@K6eJ3W@2k",
-  database: "freedb_todo_list"
+  host: "sql303.epizy.com",
+  user: "epiz_32770718",
+  password: "Esazavaj22",
+  database: "epiz_32770718_todo_list"
 });
 
 db.connect((err) => {
