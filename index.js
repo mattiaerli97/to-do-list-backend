e56d100a412ee9e0fe26f382edcc2598ab77aa35
Mattia Erli <YYI4002@infocert.it>
2022-10-11 08:24:00 +0200

    fix port

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import db from "./config.js";
 import taskRoutes from "./routes.js";
 import cors from "cors";
 
-const port = process.env.port || 8000;
+const port = process.env.PORT || 5000;
 const app = express();
 
 /* try {
